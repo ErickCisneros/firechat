f11a93f0ea267e4129a6764d324e2b0a9c8a8572
Erick Cisneros <erickxz10@gmail.com>
2019-08-10 01:35:28 -0500

    Chat final sin valor agg

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styles: []
+})
+export class ChatComponent implements OnInit {
+
+  mensaje: string = "";
+  elemento: any;
+
+  constructor(private cs: ChatService) { 
+    this.cs.cargarMensajes()
+      .subscribe(() => {
+        setTimeout(() => {
+          this.elemento.scrollTop = this.elemento.scrollHeight;
+        }, 20);
+      });
+  }
+
+  ngOnInit() {
+    this.elemento = document.getElementById('app-mensajes');
+  }
+
+  enviarMensaje() {
+    console.log(this.mensaje);
+    if(this.mensaje.length === 0) {
+      return;
+    } else {
+      this.cs.agregarMensaje(this.mensaje)
+        .then(() => this.mensaje = "")
+        .catch((err) => console.error('Error al enviar', err));
+    }
+  }
+
+}
